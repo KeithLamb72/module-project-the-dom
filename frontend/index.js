caa087f30b663a73f43a4796145c63378175f75b
KeithLamb <keithianlamb@gmail.com>
2024-04-22 15:20:57 -0500

    Finished Project

--- frontend/index.js
+++ frontend/index.js
@@ -4,24 +4,90 @@ function moduleProject1() {
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
   // 👇 WORK WORK BELOW THIS LINE 👇
-//howdy 
+
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
-  //  ✨ add your code here
+  //select all divs within section to avoid DRY
+  const widgets = document.querySelectorAll('section>div');//use console log if you want to see NodeList
+  widgets.forEach((widget, idx) => {//iterate over and add "widget" class
+    widget.classList.add('widget')
+    widget.setAttribute('tabindex', idx + 1)
+  })
 
   // 👉 TASK 2 - Build a "Quote of the Day" widget
-  //  ✨ add your code here
+  const randomIdx = Math.floor(Math.random() * quotes.length)// random index generator
+  const randomQuote = quotes[randomIdx]//makes random quote 'object' using index geneartor
+  // flesh out quote
+  const quote = document.createElement('div')//create div element to hold text of quote
+  const quoteText = randomQuote.quote//access quote property from 'randomQuote object'
+  quote.textContent = quoteText//sets the text content of 'quote' div to the text of the quote or 'quoteText'
+  const widget1 = document.querySelector('.quoteoftheday')//selects the HTML element  with the class'.quoteoftheday' which serves as container for displaying qoute
+  widget1.appendChild(quote)//adds quote div to the widget1 container
+  //flesh out author & date
+  const authorDate = document.createElement('div')//another div element to display author name and date
+  const{ author, date} = randomQuote//destructures properties from the randomQuote Object
+  authorDate.textContent = `${author} in ${date || "an unknown date"}`//set text to include author name and date if not go to"unkwown gate"
+  widget1.appendChild(authorDate)// add authorDate div to widget1 container 
 
   // 👉 TASK 3 - Build a "Corporate Speak" widget
-  //  ✨ add your code here
+  const randomVerb1 = verbs[Math.floor(Math.random() * verbs.length)]
+  const randomVerb2 = verbs[Math.floor(Math.random() * verbs.length)]
+  const randomAdverb1 = adverbs[Math.floor(Math.random() * adverbs.length)]
+  const randomAdverb2 = adverbs[Math.floor(Math.random() * adverbs.length)]
+  const randomNoun1 = nouns[Math.floor(Math.random() * nouns.length)]
+  const randomNoun2 = nouns[Math.floor(Math.random() * nouns.length)]
+  
+  const mumboJumbo = `We need to ${randomVerb1} our ${randomNoun1} ${randomAdverb1} in order to ${randomVerb2} our${randomNoun2} ${randomAdverb2}.`
+  const paragraph = document.createElement('p')
+  paragraph.textContent = mumboJumbo
+  document.querySelector('.corporatespeak').appendChild(paragraph)
 
   // 👉 TASK 4 - Build a "Countdown" widget
-  //  ✨ add your code here
+  const countdownWidget = document.querySelector('.countdown')
+  let count = 5
+  const countdown = document.createElement('p')
+  countdown.textContent = `T-minus ${count}...`
+  countdownWidget.appendChild(countdown)
 
+  const id = setInterval(() => {
+    if (count === 1) {
+      countdown.textContent = 'Liftoff!'
+      clearInterval(id)
+    } else {
+      countdown.textContent = `T-minus ${--count}...`
+    }
+   },1000)
   // 👉 TASK 5 - Build a "Friends" widget
-  //  ✨ add your code here
+  
+  const person = people[Math.floor(Math.random() * people.length)]
+  const personParagraph = document.createElement('p')
+  document.querySelector('.friends').appendChild(personParagraph)
+  const year = person.dateOfBirth.split('-')[0]
+  let sentence = `${person.fname} ${person.lname} was born in ${year} and `
 
+  if (!person.friends.length) {
+    sentence += 'has no friends.'
+  } else {
+    sentence += 'is friends with '
+    for (let idx = 0; idx < person.friends.length; idx++){
+      const friendId = person.friends[idx]
+      const friend = people.find(p => p.id === friendId)
+      const fullName = `${friend.fname} ${friend.lname}`
+      
+      let isLastIdx = idx === person.friends.length - 1
+      let isNextToLastIdx = idx === person.friends.length - 2
+      if(isLastIdx) {
+        sentence += `${fullName}.`
+      }else if (isNextToLastIdx) {
+        sentence += `${fullName} and `
+      } else {
+        sentence += `${fullName}, `
+      }
+    }
+  }
+  personParagraph.textContent = sentence
+  console.log(person)
   // 👉 TASK 6 - Make it so user can tab through the widgets
-  //  ✨ add your code here
+  
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
